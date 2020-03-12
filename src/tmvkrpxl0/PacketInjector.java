8e0ee7150b77631e80cfb088680d76d023658b0e
한현빈 <ironman466@naver.com>
2020-03-13 03:33:15 +0900

    some tab fixes(unnecessary)

--- src/tmvkrpxl0/PacketInjector.java
+++ src/tmvkrpxl0/PacketInjector.java
@@ -9,66 +9,66 @@ import net.minecraft.util.io.netty.channel.Channel;
  */
 public class PacketInjector implements PacketInjectorInterface{
 
-  private Field EntityPlayer_playerConnection;
-  private Class<?> PlayerConnection;
-  private Field PlayerConnection_networkManager;
+	private Field EntityPlayer_playerConnection;
+	private Class<?> PlayerConnection;
+	private Field PlayerConnection_networkManager;
 
-  private Class<?> NetworkManager;
-  private Field k;
-  private Field m;
+	private Class<?> NetworkManager;
+	private Field k;
+	private Field m;
 
-  public PacketInjector() {
-  try {
-  EntityPlayer_playerConnection = Reflection.getField(Reflection.getClass("{nms}.EntityPlayer"), "playerConnection");
+	public PacketInjector() {
+		try {
+			EntityPlayer_playerConnection = Reflection.getField(Reflection.getClass("{nms}.EntityPlayer"), "playerConnection");
 
-  PlayerConnection = Reflection.getClass("{nms}.PlayerConnection");
-  PlayerConnection_networkManager = Reflection.getField(PlayerConnection, "networkManager");
+			PlayerConnection = Reflection.getClass("{nms}.PlayerConnection");
+			PlayerConnection_networkManager = Reflection.getField(PlayerConnection, "networkManager");
 
-  NetworkManager = Reflection.getClass("{nms}.NetworkManager");
-  k = Reflection.getField(NetworkManager, "k");
-  m = Reflection.getField(NetworkManager, "m");
-  } catch (Throwable t) {
-  t.printStackTrace();
-  }
-  }
-  
-  @Override
-  public void addPlayer(Player p) {
-  try {
-  Channel ch = getChannel(getNetworkManager(Reflection.getNmsPlayer(p)));
-  if(ch.pipeline().get("PacketInjector") == null) {
-  PacketHandler h = new PacketHandler(p);
-  ch.pipeline().addBefore("packet_handler", "PacketInjector", h);
-  }
-  } catch (Throwable t) {
-  t.printStackTrace();
-  }
-  }
-  
-  @Override
-  public void removePlayer(Player p) {
-  try {
-  Channel ch = getChannel(getNetworkManager(Reflection.getNmsPlayer(p)));
-  if(ch.pipeline().get("PacketInjector") != null) {
-  ch.pipeline().remove("PacketInjector");
-  }
-  } catch (Throwable t) {
-      t.printStackTrace();
-  }
-  }
+			NetworkManager = Reflection.getClass("{nms}.NetworkManager");
+			k = Reflection.getField(NetworkManager, "k");
+			m = Reflection.getField(NetworkManager, "m");
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void addPlayer(Player p) {
+		try {
+			Channel ch = getChannel(getNetworkManager(Reflection.getNmsPlayer(p)));
+			if(ch.pipeline().get("PacketInjector") == null) {
+				PacketHandler h = new PacketHandler(p);
+				ch.pipeline().addBefore("packet_handler", "PacketInjector", h);
+			}
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void removePlayer(Player p) {
+		try {
+			Channel ch = getChannel(getNetworkManager(Reflection.getNmsPlayer(p)));
+			if(ch.pipeline().get("PacketInjector") != null) {
+				ch.pipeline().remove("PacketInjector");
+			}
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
 
-  private Object getNetworkManager(Object ep) {
-	  Object obj = Reflection.getFieldValue(EntityPlayer_playerConnection, ep);
-	  return Reflection.getFieldValue(PlayerConnection_networkManager, obj);
-  }
+	private Object getNetworkManager(Object ep) {
+		Object obj = Reflection.getFieldValue(EntityPlayer_playerConnection, ep);
+		return Reflection.getFieldValue(PlayerConnection_networkManager, obj);
+	}
 
-  private Channel getChannel(Object networkManager) {
-  Channel ch = null;
-  try {
-  ch = Reflection.getFieldValue(k, networkManager);
-  } catch (Exception e) {
-  ch = Reflection.getFieldValue(m, networkManager);
-  }
-  return ch;
-  }
+	private Channel getChannel(Object networkManager) {
+		Channel ch = null;
+		try {
+			ch = Reflection.getFieldValue(k, networkManager);
+		} catch (Exception e) {
+			ch = Reflection.getFieldValue(m, networkManager);
+		}
+		return ch;
+	}
 }
--- src/tmvkrpxl0/PacketInjectorNew.java
+++ src/tmvkrpxl0/PacketInjectorNew.java
@@ -11,66 +11,66 @@ import io.netty.channel.Channel;
  */
 public class PacketInjectorNew implements PacketInjectorInterface{
 
-  private Field EntityPlayer_playerConnection;
-  private Class<?> PlayerConnection;
-  private Field PlayerConnection_networkManager;
+	private Field EntityPlayer_playerConnection;
+	private Class<?> PlayerConnection;
+	private Field PlayerConnection_networkManager;
 
-  private Class<?> NetworkManager;
-  private Field k;
-  private Field m;
+	private Class<?> NetworkManager;
+	private Field k;
+	private Field m;
 
-  public PacketInjectorNew() {
-  try {
-  EntityPlayer_playerConnection = Reflection.getField(Reflection.getClass("{nms}.EntityPlayer"), "playerConnection");
+	public PacketInjectorNew() {
+	try {
+		EntityPlayer_playerConnection = Reflection.getField(Reflection.getClass("{nms}.EntityPlayer"), "playerConnection");
 
-  PlayerConnection = Reflection.getClass("{nms}.PlayerConnection");
-  PlayerConnection_networkManager = Reflection.getField(PlayerConnection, "networkManager");
+		PlayerConnection = Reflection.getClass("{nms}.PlayerConnection");
+		PlayerConnection_networkManager = Reflection.getField(PlayerConnection, "networkManager");
 
-  NetworkManager = Reflection.getClass("{nms}.NetworkManager");
-  k = Reflection.getField(NetworkManager, "k");
-  m = Reflection.getField(NetworkManager, "m");
-  } catch (Throwable t) {
-  t.printStackTrace();
-  }
-  }
-  
-  @Override
-  public void addPlayer(Player p) {
-  try {
-  Channel ch = getChannel(getNetworkManager(Reflection.getNmsPlayer(p)));
-  if(ch.pipeline().get("PacketInjector") == null) {
-  PacketHandlerNew h = new PacketHandlerNew(p);
-  ch.pipeline().addBefore("packet_handler", "PacketInjector", h);
-  }
-  } catch (Throwable t) {
-  t.printStackTrace();
-  }
-  }
+		NetworkManager = Reflection.getClass("{nms}.NetworkManager");
+		k = Reflection.getField(NetworkManager, "k");
+		m = Reflection.getField(NetworkManager, "m");
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void addPlayer(Player p) {
+		try {
+			Channel ch = getChannel(getNetworkManager(Reflection.getNmsPlayer(p)));
+			if(ch.pipeline().get("PacketInjector") == null) {
+				PacketHandlerNew h = new PacketHandlerNew(p);
+				ch.pipeline().addBefore("packet_handler", "PacketInjector", h);
+			}
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
 
-  @Override
-  public void removePlayer(Player p) {
-  try {
-  Channel ch = getChannel(getNetworkManager(Reflection.getNmsPlayer(p)));
-  if(ch.pipeline().get("PacketInjector") != null) {
-  ch.pipeline().remove("PacketInjector");
-  }
-  } catch (Throwable t) {
-      t.printStackTrace();
-  }
-  }
+	@Override
+	public void removePlayer(Player p) {
+		try {
+			Channel ch = getChannel(getNetworkManager(Reflection.getNmsPlayer(p)));
+			if(ch.pipeline().get("PacketInjector") != null) {
+				ch.pipeline().remove("PacketInjector");
+			}
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
 
-  private Object getNetworkManager(Object ep) {
-	  Object obj = Reflection.getFieldValue(EntityPlayer_playerConnection, ep);
-	  return Reflection.getFieldValue(PlayerConnection_networkManager, obj);
-  }
+	private Object getNetworkManager(Object ep) {
+		Object obj = Reflection.getFieldValue(EntityPlayer_playerConnection, ep);
+		return Reflection.getFieldValue(PlayerConnection_networkManager, obj);
+	}
 
-  private Channel getChannel(Object networkManager) {
-  Channel ch = null;
-  try {
-  ch = Reflection.getFieldValue(k, networkManager);
-  } catch (Exception e) {
-  ch = Reflection.getFieldValue(m, networkManager);
-  }
-  return ch;
-  }
+	private Channel getChannel(Object networkManager) {
+		Channel ch = null;
+		try {
+			ch = Reflection.getFieldValue(k, networkManager);
+		} catch (Exception e) {
+			ch = Reflection.getFieldValue(m, networkManager);
+		}
+		return ch;
+	}
 }
