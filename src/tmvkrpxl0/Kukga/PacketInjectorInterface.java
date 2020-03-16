557116aeab1a42512a97d1bbc49c11024b7a0d9c
한현빈 <ironman466@naver.com>
2020-03-16 13:13:02 +0900

    Scoreboard fix

--- src/tmvkrpxl0/Kukga/PacketInjectorInterface.java
+++
@@ -1,8 +0,0 @@
-package tmvkrpxl0.Kukga;
-
-import org.bukkit.entity.Player;
-
-public interface PacketInjectorInterface {
-	public void addPlayer(Player p);
-	public void removePlayer(Player p);
-}
